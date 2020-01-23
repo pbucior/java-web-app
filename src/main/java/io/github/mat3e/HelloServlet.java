42c4e05ee91c237c51dd7d7ccc4f700a0fe53418
Paweł Bucior <pawel@bucior.eu>
2020-01-23 19:25:01 +0100

    Add HelloServlet.java

---
+++ src/main/java/io/github/mat3e/HelloServlet.java
@@ -0,0 +1,20 @@
+package io.github.mat3e;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Request got");
+        resp.getWriter().write("Hello world!");
+    }
+}
