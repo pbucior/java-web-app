7c5a08eacc5d7588b4a66e6197574a0a5ef4c965
Paweł Bucior <pawel@bucior.eu>
2020-01-23 19:05:29 +0100

    Add slf4j

---
+++ src/main/java/io/github/mat3e/App.java
@@ -0,0 +1,11 @@
+package io.github.mat3e;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello World");
+    }
+}
