0bbf6ee72596c5815e7d35c65dce29e19ff5ca08
Paweł Bucior <pawel@bucior.eu>
2020-01-30 15:06:40 +0100

    Tests modification

--- src/test/java/io/github/mat3e/HelloServiceTest.java
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -2,29 +2,45 @@ package io.github.mat3e;
 
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
 
 public class HelloServiceTest {
-    private HelloService SUT = new HelloService();
-
+    private final static String WELCOME = "Hello";
     @Test
-    public void test_prepareGreeting_null_returnsGreetingWithFallback() throws Exception {
-        // given + when
-        var result = SUT.prepareGreeting(null);
+    public void test_prepareGreeting_nullName_returnsGreetingWithFallbackName() throws Exception {
+        // given
+
+        var mockRepository = alwaysReturningLangRepository();
+        var SUT = new HelloService(mockRepository);
+
+        // when
+        var result = SUT.prepareGreeting(null, "-1");
 
         // then
-        assertEquals("Hello " + HelloService.FALLBACK_NAME + "!", result);
+        assertEquals(WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
     }
 
     @Test
     public void test_prepareGreeting_name_returnsGreetingWithName() throws Exception {
         // given
+        var SUT = new HelloService();
         var name = "test";
 
         // when
-        var result = SUT.prepareGreeting(name);
+        var result = SUT.prepareGreeting(name, "-1");
 
         // then
-        assertEquals("Hello " + name + "!", result);
+        assertEquals(WELCOME + " " + name + "!", result);
+    }
+
+    private LangRepository alwaysReturningLangRepository() {
+        return new LangRepository() {
+            @Override
+            Optional<Lang> findById(Long id) {
+                return Optional.of(new Lang(null, WELCOME, null));
+            }
+        };
     }
 }
