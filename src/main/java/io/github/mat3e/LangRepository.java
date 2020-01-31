7317403789700e1b8840073346116a413c4c7e41
Paweł Bucior <pawel@bucior.eu>
2020-01-31 22:49:00 +0100

    Create LangServlet

--- src/main/java/io/github/mat3e/LangRepository.java
+++ src/main/java/io/github/mat3e/LangRepository.java
@@ -1,15 +1,25 @@
 package io.github.mat3e;
 
+import java.util.List;
 import java.util.Optional;
 
 class LangRepository {
 
+    List<Lang> findAll() {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+        var result = session.createQuery("from Lang", Lang.class).list();
+        transaction.commit();
+        session.close();
+        return result;
+    }
+
     Optional<Lang> findById(Integer id) {
         var session = HibernateUtil.getSessionFactory().openSession();
         var transaction = session.beginTransaction();
-        var result = session.get(Lang.class, id);
+        var result = Optional.ofNullable(session.get(Lang.class, id));
         transaction.commit();
         session.close();
-        return Optional.ofNullable(result);
+        return result;
     }
 }
