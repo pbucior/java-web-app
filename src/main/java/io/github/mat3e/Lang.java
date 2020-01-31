ee0a173ee17d27b079898be924e3962b350ab3f5
Paweł Bucior <pawel@bucior.eu>
2020-01-31 18:08:51 +0100

    Configure Hibernate with Lang model

--- src/main/java/io/github/mat3e/Lang.java
+++ src/main/java/io/github/mat3e/Lang.java
@@ -1,9 +1,29 @@
 package io.github.mat3e;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "languages" )
 class Lang {
+    @Id
+    @GeneratedValue(generator="inc")
+    @GenericGenerator(name="inc", strategy = "increment")
     private Integer id;
     private String welcomeMsg;
     private String code;
+
+    /**
+     * Hibernate (JPA) needs it
+     */
+    @SuppressWarnings("unused")
+    Lang() {
+
+    }
 
     public Lang(Integer id, String welcomeMsg, String code) {
         this.id = id;
