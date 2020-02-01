66bfe7dd0d9f85c242a5dcf400a4465787d3d33a
Paweł Bucior <pawel@bucior.eu>
2020-02-01 22:42:38 +0100

    Create toggleTodo method in TodoRepository

--- src/main/java/io/github/mat3e/todo/TodoRepository.java
+++ src/main/java/io/github/mat3e/todo/TodoRepository.java
@@ -4,12 +4,23 @@ import io.github.mat3e.HibernateUtil;
 import io.github.mat3e.lang.Lang;
 
 import java.util.List;
+import java.util.Optional;
 
 class TodoRepository {
     List<Todo> findAll() {
         var session = HibernateUtil.getSessionFactory().openSession();
         var transaction = session.beginTransaction();
         var result = session.createQuery("from Todo", Todo.class).list();
+        transaction.commit();
+        session.close();
+        return result;
+    }
+
+    Todo toggleTodo(Integer id) {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+        var result = session.get(Todo.class, id);
+        result.setDone(!result.isDone());
         transaction.commit();
         session.close();
         return result;
