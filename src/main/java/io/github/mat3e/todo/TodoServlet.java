d9f4a4b30285d0b88bc9c38a915ecd4575e72692
Paweł Bucior <pawel@bucior.eu>
2020-02-01 23:00:46 +0100

    Adding todos

--- src/main/java/io/github/mat3e/todo/TodoServlet.java
+++ src/main/java/io/github/mat3e/todo/TodoServlet.java
@@ -50,4 +50,10 @@ public class TodoServlet extends HttpServlet {
             logger.warn("Wrong path used: " + pathInfo);
         }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var newTodo = mapper.readValue(req.getInputStream(), Todo.class);
+        mapper.writeValue(resp.getOutputStream(), repository.addTodo(newTodo));
+    }
 }
