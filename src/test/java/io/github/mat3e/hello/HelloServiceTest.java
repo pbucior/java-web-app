1d6b91c87f0df44feb564c78c7b068bc8bdead88
Paweł Bucior <pawel@bucior.eu>
2020-02-01 22:33:46 +0100

    Refactor HelloService and HelloServlet

--- src/test/java/io/github/mat3e/hello/HelloServiceTest.java
+++ src/test/java/io/github/mat3e/hello/HelloServiceTest.java
@@ -1,6 +1,5 @@
 package io.github.mat3e.hello;
 
-import io.github.mat3e.hello.HelloService;
 import io.github.mat3e.lang.Lang;
 import io.github.mat3e.lang.LangRepository;
 import org.junit.Test;
@@ -21,7 +20,7 @@ public class HelloServiceTest {
         var SUT = new HelloService(mockRepository);
 
         // when
-        var result = SUT.prepareGreeting(null, "-1");
+        var result = SUT.prepareGreeting(null, -1);
 
         // then
         assertEquals(WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
@@ -35,7 +34,7 @@ public class HelloServiceTest {
         var name = "test";
 
         // when
-        var result = SUT.prepareGreeting(name, "-1");
+        var result = SUT.prepareGreeting(name, -1);
 
         // then
         assertEquals(WELCOME + " " + name + "!", result);
@@ -53,7 +52,7 @@ public class HelloServiceTest {
         var SUT = new HelloService(mockRepository);
 
         // when
-        var result = SUT.prepareGreeting(null, "-1");
+        var result = SUT.prepareGreeting(null, -1);
 
         // then
         assertEquals(HelloService.FALLBACK_LANG.getWelcomeMsg() + " " + HelloService.FALLBACK_NAME + "!", result);
@@ -67,19 +66,6 @@ public class HelloServiceTest {
 
         // when
         var result = SUT.prepareGreeting(null, null);
-
-        // then
-        assertEquals(FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
-    }
-
-    @Test
-    public void test_prepareGreeting_textLang_returnsGreetingWithFallbackIdLang() throws Exception {
-        // given
-        var mockRepository = fallbackLangIdRepository();
-        var SUT = new HelloService(mockRepository);
-
-        // when
-        var result = SUT.prepareGreeting(null, "abc");
 
         // then
         assertEquals(FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
