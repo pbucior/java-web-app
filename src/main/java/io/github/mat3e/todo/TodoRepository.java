a6e3a592c4fd7b9e32cad5045bbac063608e2596
Paweł Bucior <pawel@bucior.eu>
2020-02-01 22:16:48 +0100

    Create TodoRepository and TodoServlet

---
+++ src/main/java/io/github/mat3e/todo/TodoRepository.java
@@ -0,0 +1,17 @@
+package io.github.mat3e.todo;
+
+import io.github.mat3e.HibernateUtil;
+import io.github.mat3e.lang.Lang;
+
+import java.util.List;
+
+class TodoRepository {
+    List<Todo> findAll() {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+        var result = session.createQuery("from Todo", Todo.class).list();
+        transaction.commit();
+        session.close();
+        return result;
+    }
+}
